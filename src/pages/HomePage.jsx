6372ead3a77e4b0297565e4f3f6c5ef0d7b2513b
Ernest Lin <linchongtik@gmail.com>
2024-06-19 10:58:28 -0700

    add logic in Homepage, only fetch data if access token exists

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,27 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useGetUserDetailQuery } from "../features/api/apiSlice";
+import { selectCurrentToken } from "../features/auth/authSlice";
 
 function Homepage() {
-  const { data, error, isLoading, refetch } = useGetUserDetailQuery();
+  const token = useSelector(selectCurrentToken);
+  const { data, error, isLoading, refetch } = token
+    ? useGetUserDetailQuery()
+    : { data: null, error: null, isLoading: false, refetch: () => {} };
 
   useEffect(() => {
-    refetch();
-  }, []);
+    if (token) {
+      refetch();
+    }
+  }, [token]);
 
   if (isLoading) return <h1>Loading...</h1>;
-  if (error) return <h1>Error: {error.data.message}</h1>;
+  // if (error) return <h1>Error: {error.data.message}</h1>;
 
   let content;
 
   if (data) {
-    console.log(data);
+    // console.log(data);
     content = <h1>Welcome {data.username}</h1>;
   } else {
     content = <h1>Welcome, please login</h1>;
