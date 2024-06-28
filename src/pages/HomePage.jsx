36cb76a5365095ad5782cc7c840213534f573926
Ernest Lin <linchongtik@gmail.com>
2024-06-27 19:18:55 -0700

    enhance responsive layout in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -36,8 +36,11 @@ function Homepage() {
     ));
     lastestItems = (
       <>
-        <h1>Latest Products:</h1>
-        <div className="grid grid-cols-5 gap-4">{lastestItemsGrid}</div>
+        <h1 className="text-2xl font-bold">Welcome to OOTD</h1>
+        <h2 className="text-lg font-bold">Check out our latest products</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+          {lastestItemsGrid}
+        </div>
       </>
     );
   }
