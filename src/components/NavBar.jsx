f052633d30fb867369491382931059e811ad808f
Ernest Lin <linchongtik@gmail.com>
2024-06-27 17:50:58 -0700

    Implement dropdown behavior in NavBar for enhanced mobile navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,76 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectCurrentUsername } from "../features/auth/authSlice";
 import { useSendLogoutMutation } from "../features/api/apiSlice";
-import { logout } from "../features/auth/authSlice";
 
 function NavBar() {
-  const dispatch = useDispatch();
+  const [isDropdown, setIsDropdown] = useState(false);
   const username = useSelector(selectCurrentUsername);
   const [sendLogout] = useSendLogoutMutation();
   const gender = ["Men", "Women", "Unisex"];
 
+  const toggleDropdown = () => {
+    console.log("clicked");
+    setIsDropdown(!isDropdown);
+  };
   return (
     <>
       <nav className="border-b-4">
-        <div className="max-w-screen-xl mx-auto p-4 flex flex-wrap items-center justify-between">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap">
-            OOTD
-          </span>
-          <ul className="flex flex-row space-x-8 font-medium items-center">
-            <li>
-              <Link to={"/"} className="hover:text-blue-700">
+        <div className="max-w-screen-2xl mx-auto p-4 flex flex-col md:flex-row items-center justify-between text-3xl">
+          <div className="flex flex-row justify-between w-full md:w-auto">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">
+              OOTD
+            </span>
+            <button onClick={toggleDropdown} className="md:hidden">
+              <span>☰</span>
+            </button>
+          </div>
+
+          {/* flex flex-row space-x-8 font-medium items-center */}
+          <ul
+            className={`${
+              isDropdown ? "block" : "hidden"
+            }  md:block flex flex-col md:flex-row space-y-3 md:space-x-8 md:space-y-0 text-center`}
+          >
+            <li className="md:inline-block">
+              <Link
+                to={"/"}
+                className="border-b-4 border-sky-500 md:border-b-0 md:text-2xl md:font-medium hover:text-blue-700"
+              >
                 Home
               </Link>{" "}
             </li>
             {gender.map((g) => (
-              <li key={g}>
-                <Link to={`/items/${g}`} className="hover:text-blue-700">
+              <li key={g} className="md:inline-block">
+                <Link
+                  to={`/items/${g}`}
+                  className="border-b-4 border-sky-500 md:border-b-0 md:text-2xl md:font-medium hover:text-blue-700"
+                >
                   {g}
                 </Link>{" "}
               </li>
             ))}
 
-            <li>About</li>
+            <li className="md:inline-block">
+              <span className="border-b-4 border-sky-500 md:border-b-0 md:text-2xl md:font-medium hover:text-blue-700">
+                About
+              </span>
+            </li>
             {username ? (
               <>
-                <li>Wekcome, {username}</li>
-                <li>
-                  <Link to={"/Closet"} className="hover:text-blue-700">
+                <li className="md:inline-block md:text-2xl md:font-medium">
+                  Wekcome, {username}
+                </li>
+                <li className="md:inline-block">
+                  <Link
+                    to={"/Closet"}
+                    className="border-b-4 border-sky-500 md:border-b-0 md:text-2xl md:font-medium hover:text-blue-700"
+                  >
                     Your Closet
                   </Link>
                 </li>
-                <li>
+                <li className="md:inline-block">
                   {" "}
                   <button
                     onClick={() => sendLogout()}
@@ -52,13 +82,19 @@ function NavBar() {
               </>
             ) : (
               <>
-                <li>
-                  <Link to={"/register"} className="hover:text-blue-700">
+                <li className="md:inline-block">
+                  <Link
+                    to={"/register"}
+                    className="border-b-4 border-sky-500 hover:text-blue-700 md:border-b-0 md:text-2xl md:font-medium"
+                  >
                     Register
                   </Link>
                 </li>
-                <li>
-                  <Link to={"/login"} className="hover:text-blue-700">
+                <li className="md:inline-block">
+                  <Link
+                    to={"/login"}
+                    className="border-b-4 border-sky-500 hover:text-blue-700 md:border-b-0 md:text-2xl md:font-medium"
+                  >
                     Login
                   </Link>
                 </li>
